d81ed112a48cc93877ee1776b15a10d464853b1a
Mark <mark_lever@hotmail.com>
2022-09-16 16:30:52 +0100

    added ps1 file

---
+++ checkFileExists.ps1
@@ -0,0 +1,25 @@
+﻿$fileName = "C:\Sample\File14.txt"
+$doesFileExist = test-path -Path $fileName -PathType leaf
+$counter = 0
+
+if ($doesFileExist -eq $false)
+{Write-Host "$fileName doesn't exist will keep checking 10 times"}
+else
+{Write-Host "$fileName was found"}
+
+
+while($doesFileExist -eq $false -and $counter -lt 11)
+{
+    
+    Start-Sleep -Seconds 2
+    if($doesFileExist = test-path -Path $fileName -PathType leaf)
+    {
+    Write-Host "Found the file! $fileName"
+    exit
+     }
+    Write-Output "Try $counter"
+    $counter++
+    if ($counter -eq 11)
+    {Write-Host "Tried 10 times and file still not found"}
+
+}
